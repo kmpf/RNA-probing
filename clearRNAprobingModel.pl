36cee8dcfa1b6a10f1637161abdac4e81b5feab1
Christoph Kaempf <kaempf@bioinf.uni-leipzig.de>
2014-05-22 00:39:37 +0200

    Added more options to remove tertiary or Leontis-Westhof info

--- clearRNAprobingModel.pl
+++ clearRNAprobingModel.pl
@@ -46,11 +46,18 @@ require RNAprobing::BLASTresult;
 # Options section
 #
 ################################################################################
+my $rm_reactivities = 0;
+my $rm_tertiary_interactions = 0;
+my $rm_lw_base_pairs = 0;
 my $help = 0;
 my $rdf_file ="";
 my $verbose = 0;
 
+
 GetOptions(
+    "fromReactivities" => \$rm_reactivities,
+    "fromTertiaryInteractions"=> \$rm_tertiary_interactions,
+    "fromLWbasepairs"=> \$rm_lw_base_pairs,
     "help|h" => \$help,
     "rdf=s"=> \$rdf_file,
     "verbose|v+" => \$verbose);
@@ -98,25 +105,96 @@ my $parser = RDF::Trine::Parser->new( 'rdfxml' );
 my $base_uri = 'http://www.bioinf.uni-leipzig.de/~kaempf/RNAprobing.owl#';
 $parser->parse_file_into_model( $base_uri, $rdf_file, $rdf->model() );
 
-# Remove all edges with property "hasReactivity"
+## I know code below is crap style, but I thought there have been problems
+## with the parser if used in method
 
+# Remove all edges with property "hasReactivity"
+if ( $rm_reactivities != 0 ){
 # initialize hasReactivity uri
-my $has_reactivity_uri = "bioinf:hasReactivity";
+    my $has_reactivity_uri = "bioinf:hasReactivity";
 # $x needs to be undef in order to be used in the remove_statements call
-my $x;
-my $removed_stmts_count = $rdf->remove_statements($x, $has_reactivity_uri, $x);
-$logger->info("Removed ". $removed_stmts_count . " statements with property".
-              " hasReactivity");
+    my $x;
+    my $removed_stmts_count = 
+        $rdf->remove_statements($x, $has_reactivity_uri, $x);
+    $logger->info("Removed ". $removed_stmts_count . " statements with property".
+                  " hasReactivity");
 
 # Write the reduced model to file
-my $reduced_rdf = $rdf_file;
-$reduced_rdf =~ s/\.rdf$/\.reduced\.rdf/g;
-open (my $rdf_fh, ">", $reduced_rdf)  or die("Can't open $reduced_rdf.");
-my $string = $rdf->serialize( format => 'rdfxml');
+    my $reduced_rdf = $rdf_file;
+    $reduced_rdf =~ s/\.rdf$/\.wo_hasReactivity\.rdf/g;
+    open (my $rdf_fh, ">", $reduced_rdf)  or die("Can't open $reduced_rdf.");
+    my $string = $rdf->serialize( format => 'rdfxml');
 # my $string = $rdf->serialize( format => 'turtle');
 # $logger->info("$string");
-print $rdf_fh $string;
-close $rdf_fh;
+    print $rdf_fh $string;
+    close $rdf_fh;
+}
+
+# Remove all edges with property "hasTertiaryInteractionWith"
+if ( $rm_tertiary_interactions != 0 ){
+# initialize hasTertiaryInteractionWith uri
+    my $has_tertiary_interaction_with_uri = "bioinf:hasTertiaryInteractionWith";
+# $x needs to be undef in order to be used in the remove_statements call
+    my $x;
+    my $removed_stmts_count = 
+        $rdf->remove_statements($x, $has_tertiary_interaction_with_uri, $x);
+    $logger->info("Removed ". $removed_stmts_count . " statements with property".
+                  " hasTertiaryInteractionWith");
+
+# Write the reduced model to file
+    my $reduced_rdf = $rdf_file;
+    $reduced_rdf =~ s/\.rdf$/\.wo_TertiaryInteractions\.rdf/g;
+    open (my $rdf_fh, ">", $reduced_rdf)  or die("Can't open $reduced_rdf.");
+    my $string = $rdf->serialize( format => 'rdfxml');
+# my $string = $rdf->serialize( format => 'turtle');
+# $logger->info("$string");
+    print $rdf_fh $string;
+    close $rdf_fh;
+}
+
+# Remove triple with subject or object "SugarEdge" or "HoogsteenEdge"
+# the problem is that I need to delete all nodes of type SugarEdge or 
+# HoogsteenEdge as well
+if ( $rm_lw_base_pairs != 0 ){
+# initialize hoogsteenEdge and sugarEdge uri
+    my $hoogsteen_edge_uri = 'bioinf:HoogsteenEdge';
+    my $sugar_edge_uri = 'bioinf:SugarEdge';
+# $x needs to be undef in order to be used in the remove_statements call
+    my $x;
+    # Get all triples which have HoogsteenEdge as object
+    my @hoogsteen_stmts = $rdf->get_triples($x, $x, $hoogsteen_edge_uri);
+    # Get all triples which have SugarEdge as object
+    my @sugar_stmts = $rdf->get_triples($x, $x, $sugar_edge_uri);
+    # remove all stmts which start with the subject of one of the retrieved triples
+    my @stmts = ();
+    push( @stmts, (@hoogsteen_stmts, @sugar_stmts));
+    foreach (@stmts) {
+        my $removed_stmts_count = 
+            $rdf->remove_statements($_->[0], $x, $x);
+#        print($_->[0]."\n");
+        $logger->info("Removed ". $removed_stmts_count . 
+                      " statements with subject HoogsteenEdge");        
+    }
+    my $removed_stmts_count = 
+        $rdf->remove_statements($hoogsteen_edge_uri, $x, $x);
+    $logger->info("Removed ". $removed_stmts_count . " statements with subject".
+                  " HoogsteenEdge");
+    $removed_stmts_count = 
+        $rdf->remove_statements($x, $x, $sugar_edge_uri);
+    $logger->info("Removed ". $removed_stmts_count . " statements with subject".
+                  " SugarEdge");
+# Write the reduced model to file
+    my $reduced_rdf = $rdf_file;
+    $reduced_rdf =~ s/\.rdf$/\.wo_LW_Base_Pairs\.rdf/g;
+    open (my $rdf_fh, ">", $reduced_rdf)  or die("Can't open $reduced_rdf.");
+    my $string = $rdf->serialize( format => 'rdfxml');
+# my $string = $rdf->serialize( format => 'turtle');
+# $logger->info("$string");
+    print $rdf_fh $string;
+    close $rdf_fh;
+}
+
+
 
 ###############################################################
 ##
